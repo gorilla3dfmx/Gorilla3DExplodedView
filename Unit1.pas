1b471821c96a79eadfbd4e245cef14085e0ae32c
fcknbstrd <eric.berger.82@googlemail.com>
2025-07-21 13:01:57 +0200

    Initial checkin: file dropping allowed, model loading implemented and simple explosive behaviour, mouse events on sub-meshes

---
+++ Unit1.pas
@@ -0,0 +1,382 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.Math.Vectors, System.Generics.Collections,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Types3D,
+  FMX.MaterialSources,
+  Gorilla.Mesh, Gorilla.Control, Gorilla.Animation,
+  Gorilla.Transform, Gorilla.Model, Gorilla.Controller,
+  Gorilla.Controller.Passes.Environment, FMX.Controls3D, Gorilla.Light,
+  Gorilla.Viewport, Gorilla.Sphere, Gorilla.Material.Default,
+  Gorilla.Material.Custom, Gorilla.Material.Lambert, FMX.Controls.Presentation,
+  FMX.StdCtrls;
+
+type
+  TFileDropFunc = reference to function(const AFilename : String) : Boolean;
+
+  TForm1 = class(TForm)
+    GorillaViewport1: TGorillaViewport;
+    GorillaLight1: TGorillaLight;
+    GorillaRenderPassEnvironment1: TGorillaRenderPassEnvironment;
+    GorillaModel1: TGorillaModel;
+    GorillaLambertMaterialSource1: TGorillaLambertMaterialSource;
+    GorillaSphere1: TGorillaSphere;
+    GorillaLight2: TGorillaLight;
+    GorillaLight3: TGorillaLight;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    GorillaLight4: TGorillaLight;
+    GorillaLight5: TGorillaLight;
+    procedure FormCreate(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+  private
+    FActiveMesh : TGorillaMesh;
+    FAnimators : TList<TPoint3DAnimation>;
+
+    class procedure HandleFileDragOver(const AFileFilter : String;
+      const Data: TDragObject; const Point: TPointF;
+      var Operation: TDragOperation);
+    class procedure HandleFileDragDrop(const Data: TDragObject;
+      const Point: TPointF; const ACallback : TFileDropFunc);
+
+    procedure MeshClick(Sender: TObject);
+    procedure MeshMouseEnter(Sender: TObject);
+    procedure MeshMouseLeave(Sender: TObject);
+  public
+    procedure DragOver(const Data: TDragObject; const Point: TPointF;
+      var Operation: TDragOperation); override;
+    procedure DragDrop(const Data: TDragObject;
+      const Point: TPointF); override;
+
+    procedure LoadModel(const AFileName : String);
+    procedure Play();
+    procedure Reverse();
+    procedure Reset();
+  end;
+
+const
+  CurrentFilter = '*.dae;*.fbx;*.gltf;*.glb;*.obj;*.stl;*.ply;*.skp' +
+    // additional mayo formats
+    ';*.step;*.stp;*.brep;*.rle;*.occ;*.iges;*.igs;*.dxf;*.wrl;*.wrz;*.vrml;*.x;*.3mf;*.3ds;*.amf;*.off';
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+uses
+  System.Masks, System.RTLConsts, FMX.Utils,
+  Gorilla.DefTypes,
+  Gorilla.DAE.Loader,
+  Gorilla.FBX.Loader,
+  Gorilla.GLTF.Loader,
+  Gorilla.GLB.Loader,
+  Gorilla.OBJ.Loader,
+  Gorilla.STL.Loader,
+  Gorilla.PLY.Loader,
+{$IFDEF MSWINDOWS}
+  {$IFDEF CPUX64}
+  // Sketchup Import available only with Windows x64
+  Gorilla.SKP.Loader,
+  {$ENDIF}
+{$ENDIF}
+  Gorilla.Mayo.STEP.Loader,
+  Gorilla.Mayo.BREP.Loader,
+  Gorilla.Mayo.IGES.Loader,
+  Gorilla.Mayo.DXF.Loader,
+  Gorilla.Mayo.VRML.Loader,
+  Gorilla.Mayo.X.Loader,
+  Gorilla.Mayo.X3MF.Loader,
+  Gorilla.Mayo.X3DS.Loader,
+  Gorilla.Mayo.AMF.Loader,
+  Gorilla.Mayo.OFF.Loader;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  GorillaViewport1.FrustumCulling := false;
+
+  // Attach light source to designtime camera
+  GorillaLight1.Parent := GorillaViewport1.GetDesignCamera();
+  GorillaLight1.Position.Point := TPoint3D.Zero;
+
+  // Increase navigation speed
+  with GorillaViewport1.GetDesignCameraController() do
+  begin
+    Interval := 10;
+    ShiftIntensity := 5;
+    IntensityRotate := 25;
+    IntensityZoom := 15;
+  end;
+end;
+
+procedure TForm1.LoadModel(const AFileName : String);
+const
+  EXPLOSIVE_DURATION = 2;
+  EXPLOSIVE_STRENGTH = 1;
+
+  procedure SetupAnimators(AMesh : TGorillaMesh);
+  var LAnim : TPoint3DAnimation;
+      LModelCenter, LDir,
+      LMeshCenterPos : TPoint3D;
+      I : Integer;
+      LLen : Single;
+      LDef : TMeshDef;
+      LBBox : TBoundingBox;
+  begin
+    // Only set up an animator if there are triangles to be moved
+    if Assigned(AMesh.Def) then
+    begin
+      LDef := AMesh.Def as TMeshDef;
+      if Assigned(LDef.IndexSource) and (LDef.IndexSource.Length > 0) then
+      begin
+        // Assign mouse callbacks
+        AMesh.HitTest := true;
+        AMesh.OnClick := MeshClick;
+        AMesh.OnMouseEnter := MeshMouseEnter;
+        AMesh.OnMouseLeave := MeshMouseLeave;
+
+        // Calculate distance to model center to get the direction
+        LBBox := GorillaModel1.GetVertexDataBoundingBox(TMatrix3D.Identity, false);
+        LModelCenter := TPoint3D.Zero;
+
+        LBBox := LDef.GetCombinedBoundingBox();
+        LBBox := LDef.GetBoundingBox(TMatrix3D.Identity, false, true, false);
+        LMeshCenterPos := LBBox.CenterPoint;
+        LLen := LMeshCenterPos.Distance(LModelCenter);
+        LDir := (LMeshCenterPos - LModelCenter);
+        LDir := LDir.Normalize();
+
+        LAnim := TPoint3DAnimation.Create(AMesh);
+        LAnim.Parent := AMesh;
+        LAnim.PropertyName := 'Position.Point';
+        LAnim.Duration := EXPLOSIVE_DURATION;
+        LAnim.StartValue.Point := AMesh.Position.Point;
+        LAnim.StopValue.Point := LAnim.StartValue.Point + (LDir * EXPLOSIVE_STRENGTH);
+
+        FAnimators.Add(LAnim);
+      end;
+    end;
+
+    if not Assigned(AMesh.Meshes) then
+      Exit;
+
+    for I := 0 to AMesh.Meshes.Count - 1 do
+      SetupAnimators(AMesh.Meshes.Items[I]);
+  end;
+
+begin
+  // Reset model, animators and environment render pass
+  FreeAndNil(FAnimators);
+  GorillaModel1.Clear();
+  GorillaRenderPassEnvironment1.IgnoredControls.Clear();
+  GorillaRenderPassEnvironment1.AllowedControls.Clear();
+
+  // Load model
+  GorillaModel1.LoadFromFile(nil, AFilename, []);
+//  GorillaModel1.Scale.Point := Point3D(10, 10, 10);
+  GorillaModel1.Position.Y := -5;
+  GorillaModel1.RotationAngle.X := 180;
+
+  // Disable mouse interaction generally
+  // In SetupAnimators we will allow mouse interaction on specific meshes
+  GorillaModel1.SetHitTestValue(false);
+
+  // Apply environment render pass
+  GorillaRenderPassEnvironment1.IsDynamic := true;
+  GorillaRenderPassEnvironment1.IgnoreControl(GorillaModel1);
+  GorillaModel1.SetEnvironmentMapping(GorillaRenderPassEnvironment1, 1, 0);
+
+  FAnimators := TList<TPoint3DAnimation>.Create();
+  SetupAnimators(GorillaModel1);
+end;
+
+procedure TForm1.Play();
+var I : Integer;
+begin
+  if not Assigned(FAnimators) then
+    Exit;
+
+  Reset();
+  for I := 0 to FAnimators.Count - 1 do
+  begin
+    FAnimators[I].Start();
+  end;
+end;
+
+procedure TForm1.Reverse();
+var I : Integer;
+begin
+  if not Assigned(FAnimators) then
+    Exit;
+
+  for I := 0 to FAnimators.Count - 1 do
+  begin
+    FAnimators[I].Enabled := false;
+    FAnimators[I].Stop();
+    FAnimators[I].Inverse := true;
+    FAnimators[I].StartFromCurrent := true;
+    FAnimators[I].Enabled := true;
+    FAnimators[I].Start();
+  end;
+end;
+
+procedure TForm1.Reset();
+var I : Integer;
+begin
+  if not Assigned(FAnimators) then
+    Exit;
+
+  for I := 0 to FAnimators.Count - 1 do
+  begin
+    FAnimators[I].Stop();
+    FAnimators[I].Enabled := false;
+    FAnimators[I].Inverse := false;
+    TGorillaMesh(FAnimators[I].Parent).Position.Point := FAnimators[I].StartValue.Point;
+  end;
+
+  GorillaViewport1.Repaint;
+end;
+
+class procedure TForm1.HandleFileDragOver(const AFileFilter : String;
+  const Data: TDragObject; const Point: TPointF; var Operation: TDragOperation);
+var
+  Masks, M: string;
+  HasFiles: Boolean;
+  HasFilter: Boolean;
+  HasMatchedFile: Boolean;
+  I: Integer;
+begin
+  inherited;
+
+  // determine if the user is dragging one or more files and
+  // if there is any filter set
+  HasFiles := Length(Data.Files) > 0;
+  Masks := AFileFilter;
+  HasFilter := Masks <> '';
+
+  // the Accept value is overriden by the filter only if there is at least one file
+  // in the Data drag object; when a filter exists, there must be at least
+  // one file that matches the filter in order for Accept to have set by the user;
+  // if there is no file matching the filter, Accept is false
+  if HasFiles and HasFilter then
+  begin
+    HasMatchedFile := False;
+    M := GetToken(Masks, ';');
+    while (M <> '') and (not HasMatchedFile) do
+    begin
+      for I := 0 to High(Data.Files) do
+      begin
+        HasMatchedFile := MatchesMask(Data.Files[I], M);
+
+        // there is at least one file matching the filter
+        if HasMatchedFile then
+          Break;
+      end;
+      M := GetToken(Masks, ';');
+    end;
+
+    if HasMatchedFile then
+      Operation := TDragOperation.Move;
+  end;
+end;
+
+class procedure TForm1.HandleFileDragDrop(const Data: TDragObject;
+  const Point: TPointF; const ACallback : TFileDropFunc);
+var I : Integer;
+    LFileName : String;
+begin
+  inherited;
+
+  for I := 0 to High(Data.Files) do
+  begin
+    try
+      LFileName := Data.Files[I];
+
+      if Assigned(ACallback) then
+      begin
+        if not ACallback(LFileName) then
+          Continue;
+      end;
+    except
+      on E: Exception do
+      begin
+        raise Exception.CreateFmt(
+          'Failed to load 3D model file: %s'#13#10'ERROR: %s',
+          [LFileName, E.Message]);
+      end;
+    end;
+  end;
+end;
+
+procedure TForm1.DragOver(const Data: TDragObject;
+  const Point: TPointF; var Operation: TDragOperation);
+begin
+  inherited;
+
+  HandleFileDragOver(CurrentFilter, Data, Point, Operation);
+end;
+
+procedure TForm1.DragDrop(const Data: TDragObject;
+  const Point: TPointF);
+begin
+  inherited;
+
+  HandleFileDragDrop(Data, Point,
+    function(const AFileName : String) : Boolean
+    begin
+      LoadModel(AFileName);
+      Result := true;
+    end);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Play();
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Reverse();
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  Reset();
+end;
+
+procedure TForm1.MeshClick(Sender: TObject);
+var LMesh : TGorillaMesh;
+begin
+  LMesh := Sender as TGorillaMesh;
+
+  if Assigned(FActiveMesh) then
+    FActiveMesh.IsMarked := false;
+
+  FActiveMesh := LMesh;
+  FActiveMesh.EnableDesigning();
+  FActiveMesh.IsMarked := true;
+end;
+
+procedure TForm1.MeshMouseEnter(Sender: TObject);
+var LMesh : TGorillaMesh;
+begin
+  LMesh := Sender as TGorillaMesh;
+  LMesh.Wireframe := true;
+end;
+
+procedure TForm1.MeshMouseLeave(Sender: TObject);
+var LMesh : TGorillaMesh;
+begin
+  LMesh := Sender as TGorillaMesh;
+  LMesh.Wireframe := false;
+end;
+
+end.
